64ab18ffe6b4c1d84aa670cc9dd3cb6b0e26040b
Michael <qwentin536@gmail.com>
2025-09-18 15:44:57 +0300

    feat: add sumOfCodes 2025-09-18 15:44:57

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -200,8 +200,16 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let result = 0;
+
+  if (str) {
+    for (let i = 0; i < str.length; i += 1) {
+      result += str.charCodeAt(i);
+    }
+  }
+
+  return result;
 }
 
 /**
