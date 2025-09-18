411505ce2895336f93e6e08261390201d6385f03
Michael <qwentin536@gmail.com>
2025-09-19 00:01:30 +0300

    feat: add findLongestWord 2025-09-19 00:01:30

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -362,8 +362,16 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  let longestWord = '';
+
+  sentence.split(' ').forEach((word) => {
+    if (longestWord.length < word.length) {
+      longestWord = word;
+    }
+  });
+
+  return longestWord;
 }
 
 /**
